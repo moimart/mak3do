8daa25cfabad7c4df1a7c7a30bfa37b4d7251764
Moises Martinez <moimart@gmail.com>
2020-02-18 17:52:07 +0100

    Improve framework handling

--- mak3do-sandbox/main.m
+++ mak3do-sandbox/main.m
@@ -7,7 +7,7 @@
 //
 
 #import <UIKit/UIKit.h>
-#import <mak3do/all.h>
+#import <mak3do/all>
 
 int main(int argc, char * argv[]) {
     NSString * appDelegateClassName;
--- mak3do/all.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-//#include "mak3do/game/all"
-//#include "mak3do/scenegraph/all"
-#include "mak3do/rendering/apple/AppDelegate.h"
-
