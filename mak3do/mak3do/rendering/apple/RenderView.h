617693b7b2fe9ceed347e18e0710e3249ec39051
Moises Martinez <moimart@gmail.com>
2020-03-07 18:06:41 +0100

    Support basic mouse events on mac

--- mak3do/mak3do/rendering/apple/RenderView.h
+++ mak3do/mak3do/rendering/apple/RenderView.h
@@ -2,5 +2,5 @@
 #import <MetalKit/MetalKit.h>
 
 @interface RenderView : MTKView
-- (nonnull instancetype)initWithFrame:(CGRect)frameRect device:(nullable id<MTLDevice>)device NS_DESIGNATED_INITIALIZER;
+- (nonnull instancetype)initWithFrame:(CGRect)frameRect device:(nullable id<MTLDevice>)device;
 @end
--- mak3do/mak3do/rendering/apple/RenderView.mm
+++ mak3do/mak3do/rendering/apple/RenderView.mm
@@ -9,13 +9,41 @@
 - (nonnull instancetype)initWithFrame:(CGRect)frameRect device:(nullable id<MTLDevice>)device {
     
     self = [super initWithFrame:frameRect device:device];
-    
+#if TARGET_OS_IOS
     self.multipleTouchEnabled = YES;
-    
+#endif
     _pimpl = mak3do::io::TouchManager::get()->pimpl().get();
     
     return self;
 }
+
+#if TARGET_OS_MAC
+- (void) mouseDown:(NSEvent *)event {
+    auto __touch = std::make_shared<mak3do::io::Touch>();
+    __touch->location.x = [event locationInWindow].x;
+    __touch->location.y = [event locationInWindow].y;
+    
+    _pimpl->inject_touches_begin({__touch});
+}
+
+- (void) mouseDragged:(NSEvent *)event {
+    auto __touch = std::make_shared<mak3do::io::Touch>();
+    __touch->location.x = [event locationInWindow].x;
+    __touch->location.y = [event locationInWindow].y;
+    
+    _pimpl->inject_touches_moved({__touch});
+}
+
+- (void) mouseUp:(NSEvent *)event {
+    auto __touch = std::make_shared<mak3do::io::Touch>();
+    __touch->location.x = [event locationInWindow].x;
+    __touch->location.y = [event locationInWindow].y;
+    
+    _pimpl->inject_touches_ended({__touch});
+}
+
+#endif
+
 #if TARGET_OS_IOS
 - (void) touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
     std::vector<mak3do::io::TouchPtr> _touches;
